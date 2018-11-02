e6e4223b76bd0b7e70de6a126e1a2c95fb504a26
Shaw <cuzval@gmail.com>
2018-11-02 17:01:48 +0800

    Prevent user interaction during transitioning.

--- Sources/_RRPushAnimator.m
+++ Sources/_RRPushAnimator.m
@@ -2,8 +2,26 @@
 //  _RRPushAnimator.m
 //  RRNavigationTransitioning
 //
-//  Created by Shaw on 10/16/18.
-//  Copyright © 2018 RedRain. All rights reserved.
+//  Created by Shaw <cuzval@gmail.com> on 10/16/18.
+//  Copyright (c) 2018 RedRain.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #import "_RRPushAnimator.h"
@@ -21,10 +39,15 @@ UIViewAnimationOptions const  _RRViewAnimationOptionCurveKeyboard = 7 << 16;
     UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     if (!fromVC || !toVC) {
+        [transitionContext cancelInteractiveTransition];
+        [transitionContext completeTransition:NO];
         return;
     }
 
+    fromVC.view.userInteractionEnabled = NO;
+    toVC.view.userInteractionEnabled = NO;
     [toVC.view layoutIfNeeded];
+    
     UIView *containerView = transitionContext.containerView;
     [containerView addSubview:toVC.view];
     toVC.view.transform = CGAffineTransformMakeTranslation(containerView.bounds.size.width, 0);
@@ -45,10 +68,21 @@ UIViewAnimationOptions const  _RRViewAnimationOptionCurveKeyboard = 7 << 16;
                          if ([containerView.subviews containsObject:tabBar]) {
                              [tabBarController.view addSubview:tabBar];
                          }
+                         
                          if (tabBar._rr_pushing) {
                              tabBar._rr_pushing = NO;
                          }
-                         [transitionContext completeTransition:!transitionContext.transitionWasCancelled];
+                         
+                         fromVC.view.userInteractionEnabled = YES;
+                         toVC.view.userInteractionEnabled = YES;
+                         
+                         if (transitionContext.transitionWasCancelled) {
+                             [transitionContext cancelInteractiveTransition];
+                             [transitionContext completeTransition:NO];
+                         } else {
+                             [transitionContext finishInteractiveTransition];
+                             [transitionContext completeTransition:YES];
+                         }
                      }];
 }
 
