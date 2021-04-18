ebf75b17fa5356ee5dedc6fb97d922a6abd94829
Shaw <cuzval@gmail.com>
2021-04-18 12:52:55 +0800

    Support SMP

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RRNavigationTransitioning",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(name: "RRNavigationTransitioning", targets: ["RRNavigationTransitioning"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "RRNavigationTransitioning", dependencies: [], path: "Sources")
+    ]
+)
