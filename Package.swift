8de91cab0fdb8af82617d567759d3010a7fceb5b
Shaw <cuzval@gmail.com>
2021-05-13 11:05:42 +0800

    Fix Xcode 12.5 Swift Package Manager issue

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
             exclude: [
                 "Info.plist",
             ],
-            publicHeadersPath: "",
+            publicHeadersPath: "Public",
             cSettings: [
                 .headerSearchPath("Internal"),
             ]
